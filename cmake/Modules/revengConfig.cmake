2a7546de7beda656fd3d7fad3d24b4d046de7963
Tim K <tkuester@users.noreply.github.com>
2020-05-10 20:17:09 -0400

    Migrating CMake Modules

--- cmake/Modules/revengConfig.cmake
+++ cmake/Modules/revengConfig.cmake
@@ -22,9 +22,10 @@ FIND_LIBRARY(
           /usr/local/lib64
           /usr/lib
           /usr/lib64
-)
+          )
+
+include("${CMAKE_CURRENT_LIST_DIR}/revengTarget.cmake")
 
 INCLUDE(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(REVENG DEFAULT_MSG REVENG_LIBRARIES REVENG_INCLUDE_DIRS)
 MARK_AS_ADVANCED(REVENG_LIBRARIES REVENG_INCLUDE_DIRS)
-
